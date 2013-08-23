e363eea34568c82b7ddab9bc6df126c18810da9b
Oliver Palmer <oliverpalmer@opalmer.com>
2013-08-22 23:56:54 -0400

    adding some improvements for sqlite

--- pyfarm/models/core/app.py
+++ pyfarm/models/core/app.py
@@ -17,9 +17,24 @@
 import uuid
 from flask import Flask
 from pyfarm.core.config import cfg
-from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask("PyFarm")
 app.config["SQLALCHEMY_DATABASE_URI"] = cfg.get("db.uri")
 app.secret_key = str(uuid.uuid4())  # TODO: this needs a config or extern lookup
+
+# sqlite fixes (development work only)
+if cfg.get("db.uri").startswith("sqlite"):
+    from sqlalchemy.engine import Engine
+    from sqlalchemy import event
+
+    @event.listens_for(Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.execute("PRAGMA synchronous=OFF")
+        cursor.execute("PRAGMA journal_mode=MEMORY")
+        cursor.close()
+
+
+from flask.ext.sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
