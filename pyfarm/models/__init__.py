fe0f6440977f9f9cda85bf1f8a8d0ea23fa8cc9a
Oliver Palmer <oliverpalmer@opalmer.com>
2013-09-01 18:35:44 -0400

    removing top level imports since they often seem to produce problems, consumers will have to be more explicit/verbose with their own imports now

--- pyfarm/models/__init__.py
+++ pyfarm/models/__init__.py
@@ -33,12 +33,3 @@ else:
 
 if cfg.get("db.uri").startswith("sqlite"):
     warn("sqlite is for development purposes only", ConfigurationWarning)
-
-# load the interface classes, this also loads the models to
-# define relationships
-from pyfarm.models.agent import Agent, AgentSoftware, AgentTag
-from pyfarm.models.job import Job, JobSoftware, JobTag
-from pyfarm.models.task import Task
-
-__all__ = ["Agent", "AgentSoftware", "AgentTag",
-           "Job", "JobSoftware", "JobTag", "Task"]
