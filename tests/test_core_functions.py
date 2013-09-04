561bfb38dca3d0ece5a637d5c1b6f7d6ad27f394
Oliver Palmer <oliverpalmer@opalmer.com>
2013-09-03 23:57:18 -0400

    initial commit

---
+++ tests/test_core_functions.py
@@ -0,0 +1,25 @@
+# No shebang line, this module is meant to be imported
+#
+# Copyright 2013 Oliver Palmer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from utcore import ModelTestCase
+from pyfarm.models.core.functions import modelfor
+
+
+class TestFunctionsModule(ModelTestCase):
+    def test_modelfor(self):
+        class Foo(object):
+            __tablename__ = "test"
+        self.assertTrue(modelfor(Foo(), Foo.__tablename__))
