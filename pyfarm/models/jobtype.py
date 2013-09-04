c14d01af4facc6ad4206769aa1211a63a7614cb9
Oliver Palmer <oliverpalmer@opalmer.com>
2013-09-03 21:37:37 -0400

    initial documentation

--- pyfarm/models/jobtype.py
+++ pyfarm/models/jobtype.py
@@ -23,6 +23,9 @@ to an individual job.  See :mod:`pyfarm.models.job` for more the more
 general implementation.
 """
 
+from textwrap import dedent
+from sqlalchemy.orm import validates
+from pyfarm.core.enums import JobTypeLoadMode
 from pyfarm.models.core.cfg import TABLE_JOB_TYPE, MAX_JOBTYPE_LENGTH
 from pyfarm.models.core.app import db
 
@@ -33,7 +36,46 @@ class JobTypeModel(db.Model):
     """
     __tablename__ = TABLE_JOB_TYPE
 
-    name = db.Column(db.String(MAX_JOBTYPE_LENGTH), nullable=False)
-    code = db.Column(db.Text)
-    # loadmode =
-    #
+    name = db.Column(db.String(MAX_JOBTYPE_LENGTH), nullable=False,
+                     doc=dedent("""
+                     The name of the job type.  This can be either a human
+                     readable name or the name of the job type class
+                     itself."""))
+    description = db.Column(db.Text, nullable=True,
+                            doc=dedent("""
+                            Human readable description of the job type.  This
+                            field is not required and is not directly relied
+                            upon anywhere."""))
+    classname = db.Column(db.String(MAX_JOBTYPE_LENGTH), nullable=True,
+                          doc=dedent("""
+                          The name of the job class contained within the file
+                          being loaded.  This field may be null but when it's
+                          not provided :attr:`name` will be used instead."""))
+    code = db.Column(db.UnicodeText, nullable=False,
+                     doc=dedent("""
+                     General field containing the 'code' the retrieve the job
+                     type.  See below for information on what this field will
+                     contain depending on how the job will be loaded.
+
+                     * DOWNLOAD - full source code for the job type
+                     * IMPORT - full path to the module to import
+                     * OPEN - remote filepath to open (this is *not*
+                     cross-platform safe)"""))
+    mode = db.Column(db.Integer, default=JobTypeLoadMode.IMPORT, nullable=False,
+                     doc=dedent("""
+                     Indicates how the job type should be retrieved.
+
+                     .. csv-table:: **JobTypeLoadMode Enums**
+                        :header: Value, Result
+                        :widths: 10, 50
+
+                        DOWNLOAD, job type will be downloaded remotely
+                        IMPORT, the remote agent will import the job type
+                        OPEN, code is loaded directly from a file on disk"""))
+
+    @validates("mode")
+    def validates_mode(self, key, value):
+        """ensures the value provided to :attr:`mode` is valid"""
+        if value not in JobTypeLoadMode:
+            raise ValueError("invalid value for mode")
+        return value
