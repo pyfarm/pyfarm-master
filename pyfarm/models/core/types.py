e6d05827b4c686493b514b69124e844543fa6dcb
Oliver Palmer <oliverpalmer@opalmer.com>
2013-09-06 23:26:14 -0400

    impl override does not make sense here (overrding load_dialect_impl)

--- pyfarm/models/core/types.py
+++ pyfarm/models/core/types.py
@@ -72,8 +72,6 @@ class GUID(TypeDecorator):
         This code is copied from sqlalchemy's standard documentation with
         some minor modifications
     """
-    impl = CHAR
-
     def load_dialect_impl(self, dialect):
         # Currently, pg8000 does not support the PGUuid type.  This is
         # backed up both by tests and from sqlalchemy's docs. Unfortunately,
