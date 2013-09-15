a6dd9d835e1fd428c984c7faee95f5f65a4655b9
Oliver Palmer <oliverpalmer@opalmer.com>
2013-09-14 23:43:53 -0400

    added tag tests

--- tests/test_model_job.py
+++ tests/test_model_job.py
@@ -13,3 +13,55 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from sqlalchemy.exc import DatabaseError
+from utcore import ModelTestCase
+from pyfarm.models.core.app import db
+from pyfarm.models.job import JobTagsModel, JobModel
+
+
+class TestTags(ModelTestCase):
+    def test_tag_insert(self):
+        job = JobModel()
+        tag = JobTagsModel()
+        tag.job = job
+        tag.tag = "foo"
+        db.session.add_all([tag, job])
+        db.session.commit()
+        model_id = tag.id
+        job_id = job.id
+        db.session.remove()
+        result = JobTagsModel.query.filter_by(id=model_id).first()
+        self.assertEqual(result.tag, "foo")
+        self.assertEqual(result.job.id, job_id)
+
+    def test_null_values(self):
+        with self.assertRaises(DatabaseError):
+            model = JobTagsModel()
+            db.session.add(model)
+            db.session.commit()
+
+        db.session.remove()
+
+        with self.assertRaises(DatabaseError):
+            tag = JobTagsModel()
+            tag.tag = "foo"
+            db.session.add(model)
+            db.session.commit()
+
+    def test_tag_unique(self):
+        job = JobModel()
+        tagA = JobTagsModel()
+        tagB = JobTagsModel()
+        tagA.job = job
+        tagA.tag = "foo"
+        tagB.job = job
+        tagB.tag = "foo"
+        db.session.add_all([job, tagA, tagB])
+
+        with self.assertRaises(DatabaseError):
+            db.session.commit()
+
+
+class TestSoftware(ModelTestCase):
+    pass
