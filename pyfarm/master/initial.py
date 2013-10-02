7109250d1c9d656c4f10273596e424fc489d26e5
Oliver Palmer <oliverpalmer@opalmer.com>
2013-10-02 01:38:24 -0400

    first usable /setup/ page

--- pyfarm/master/initial.py
+++ pyfarm/master/initial.py
@@ -20,7 +20,10 @@ Initial Setup
 
 Entry points for the /setup/ target
 """
-from flask import render_template
+
+from wtforms import Form, TextField, PasswordField, ValidationError
+from wtforms.validators import Required
+from flask import render_template, request
 from pyfarm.master.application import db
 
 # import all tables so we know what should exist and
@@ -33,24 +36,48 @@ from pyfarm.models.agent import AgentModel, AgentSoftwareModel, AgentTagsModel
 from pyfarm.models.users import User, Role
 
 
+class NewUserForm(Form):
+    username = TextField(validators=[Required()])
+    email = TextField(validators=[Required()])
+    password = PasswordField(validators=[Required()])
+
+    def validate_username(self, field):
+        user = User.get(request.form["username"])
+        if user is not None:
+            raise ValidationError(
+                "%s already exists" % request.form["username"])
+
+
 def setup_page():
-    # make sure the tables exist
-    db.create_all()
+    form = NewUserForm(request.form)
 
-    # create the admin role if it does not exist and
-    # find any existing administrators
-    admin = Role.query.filter_by(name="admin").first()
-    if admin is None:
-        admin = Role.create("admin")
-        user = User.create("admin2", "password")
-        user.roles.append(admin)
-        db.session.add(user)
-        db.session.commit()
-        admin_users = []
-    else:
-        admin_users = admin.users
+    if request.method == "GET":
+        # make sure the tables exist
+        db.create_all()
 
-    #User.create("admin", "password", roles=set(["admin"]))
+        # create the admin role if it does not exist and
+        # find any existing administrators
+        admin_role = Role.query.filter_by(name="admin").first()
+        if admin_role is None:
+            admin_role = Role.create("admin")
+            admin_users = []
+        else:
+            admin_users = admin_role.users
 
-    return render_template("pyfarm/setup.html",
-                           admin_exists=admin, admin_users=admin_users)
+        return render_template("pyfarm/setup.html", form=form,
+                               admin_role=admin_role, admin_users=admin_users)
+
+    elif request.method == "POST":
+        if form.validate():
+            admin_role = Role.create("admin")
+            user = User.create(
+                request.form["username"],
+                request.form["password"],
+                email=request.form["email"])
+            user.roles.append(admin_role)
+            db.session.add(user)
+            db.session.commit()
+            return render_template("pyfarm/setup.html",
+                                   finished=True, redirect_seconds=5)
+
+        return render_template("pyfarm/setup.html", form=form)
