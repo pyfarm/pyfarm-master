9e25ef88a656786f437cc6acc0e1be0ffa132d85
Oliver Palmer <oliverpalmer@opalmer.com>
2013-10-07 21:20:34 -0400

    removing get_list override (actually not necessary...)

--- pyfarm/master/admin/base.py
+++ pyfarm/master/admin/base.py
@@ -22,10 +22,7 @@ Setup the administrative index.
 """
 
 
-from sqlalchemy.orm import joinedload
-from sqlalchemy import or_
 from flask import redirect, abort
-from flask.ext.admin.contrib.sqla import tools
 from flask.ext.login import current_user, current_app
 from flask.ext.admin.contrib import sqla
 from flask.ext.admin import AdminIndexView
@@ -118,92 +115,3 @@ class BaseModelView(AuthMixins, sqla.ModelView):
             return self.edit_form_class
         else:
             return self.get_form()
-
-    def get_list(self, page, sort_column, sort_desc, search, filters,
-                 execute=True):
-        """
-        Override of :meth:`sqla.ModelView.get_list` which uses count() instead
-        of scalar() to determine the number of results.  This generally
-        prevents the majority of problems scalar() creates with using
-        a relationship column in a query.
-        """
-
-        # Will contain names of joined tables to avoid duplicate joins
-        joins = set()
-
-        query = self.get_query()
-        count_query = self.get_count_query()
-
-        # Apply search criteria
-        if self._search_supported and search:
-            # Apply search-related joins
-            if self._search_joins:
-                for jn in self._search_joins.values():
-                    query = query.join(jn)
-                    count_query = count_query.join(jn)
-
-                joins = set(self._search_joins.keys())
-
-            # Apply terms
-            terms = search.split(' ')
-
-            for term in terms:
-                if not term:
-                    continue
-
-                stmt = tools.parse_like_term(term)
-                filter_stmt = [c.ilike(stmt) for c in self._search_fields]
-                query = query.filter(or_(*filter_stmt))
-                count_query = count_query.filter(or_(*filter_stmt))
-
-        # Apply filters
-        if filters and self._filters:
-            for idx, value in filters:
-                flt = self._filters[idx]
-
-                # Figure out joins
-                tbl = flt.column.table.name
-
-                join_tables = self._filter_joins.get(tbl, [])
-
-                for table in join_tables:
-                    if table.name not in joins:
-                        query = query.join(table)
-                        count_query = count_query.join(table)
-                        joins.add(table.name)
-
-                # Apply filter
-                query = flt.apply(query, value)
-                count_query = flt.apply(count_query, value)
-
-        # Calculate number of rows
-        count = count_query.count()
-
-        # Auto join
-        for j in self._auto_joins:
-            query = query.options(joinedload(j))
-
-        # Sorting
-        if sort_column is not None:
-            if sort_column in self._sortable_columns:
-                sort_field = self._sortable_columns[sort_column]
-
-                query, joins = self._order_by(query, joins, sort_field, sort_desc)
-        else:
-            order = self._get_default_order()
-
-            if order:
-                query, joins = self._order_by(query, joins, order[0], order[1])
-
-        # Pagination
-        if page is not None:
-            query = query.offset(page * self.page_size)
-
-        query = query.limit(self.page_size)
-
-        # Execute if needed
-        if execute:
-            query = query.all()
-
-        return count, query
-
