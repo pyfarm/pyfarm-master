4840c3234f97c884363ac694a829c57313b4170d
Oliver Palmer <oliverpalmer@opalmer.com>
2013-10-14 21:11:26 -0400

    initial docs

--- pyfarm/master/api/agents.py
+++ pyfarm/master/api/agents.py
@@ -16,7 +16,7 @@
 
 from flask.views import MethodView
 from pyfarm.models.agent import AgentModel
-from pyfarm.master.api.decorators import put_model
+from pyfarm.master.api.decorators import put_model, post_model
 from pyfarm.master.application import db
 from pyfarm.master.utility import JSONResponse
 
@@ -40,10 +40,99 @@ class AgentsIndex(MethodView):
         db.session.commit()
         return JSONResponse(model.to_dict())
 
-    # TODO: decorator
-    # TODO: id MUST be provided
+
     # TODO: be sure not to null non-nullables by providing a null
     # TODO: if STATE is changed to offline, STOP other tasks/related activity
+    @post_model(AgentModel)
     def post(self, data, model):
+        """
+        .. http:post:: /agents
 
-        pass
+            * update column (or columns)
+
+                **Request**
+
+                .. sourcecode:: http
+
+                    POST /<prefix>/agents HTTP/1.1
+                    Accept: application/json
+
+                    {"id": 2, "ram": 256}
+
+                **Response**
+
+                .. sourcecode:: http
+
+                    HTTP/1.1 200 OK
+                    Content-Type: application/json
+
+                    {
+                        "cpu_allocation": 1.0,
+                        "cpus": 4,
+                        "freeram": 2294,
+                        "hostname": "agent",
+                        "id": 2,
+                        "ip": "10.190.195.156",
+                        "port": 16207,
+                        "ram": 256,
+                        "ram_allocation": 0.8,
+                        "state": 8
+                     }
+
+            * update unknown column(s)
+
+                **Request**
+
+                .. sourcecode:: http
+
+                    POST /<prefix>/agents HTTP/1.1
+                    Accept: application/json
+
+                    {"id": 2, "foo": 256}
+
+                **Response**
+
+                .. sourcecode:: http
+
+                    HTTP/1.1 400 BAD REQUEST
+                    Content-Type: application/json
+
+                    [5, "unknown columns were included with the request: ['foo']"]
+
+            * update without ID
+
+                **Request**
+
+                .. sourcecode:: http
+
+                    POST /<prefix>/agents HTTP/1.1
+                    Accept: application/json
+
+                    {"ram": 256}
+
+                **Response**
+
+                .. sourcecode:: http
+
+                    HTTP/1.1 400 BAD REQUEST
+                    Content-Type: application/json
+
+                    [2, "id field is missing"]
+        """
+        update_data = data.copy()
+        update_data.pop("id")
+
+        # add the model to the session and update it
+        updated = False
+        db.session.add(model)
+        for key, value in update_data.iteritems():
+            current_value = getattr(model, key)
+
+            if value != current_value:
+                setattr(model, key, value)
+                updated = True
+
+        if updated:
+            db.session.commit()
+
+        return JSONResponse(model.to_dict())
