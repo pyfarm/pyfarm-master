1245b5621aafe72efffd17ebc6a2137472ce1e95
Oliver Palmer <oliverpalmer@opalmer.com>
2013-10-17 00:17:15 -0400

    adding POST for /agents

--- pyfarm/master/api/agents.py
+++ pyfarm/master/api/agents.py
@@ -25,41 +25,192 @@ Contained within this module are API handling functions which can
 """
 
 from functools import partial
-from httplib import NOT_FOUND, NO_CONTENT, OK
+from httplib import NOT_FOUND, NO_CONTENT, OK, CREATED, BAD_REQUEST
 from flask import request
 from flask.views import MethodView
+from pyfarm.core.logger import getLogger
 from pyfarm.core.enums import APIError
 from pyfarm.models.agent import AgentModel
 from pyfarm.master.application import db
-from pyfarm.master.utility import JSONResponse, column_cache, json_from_request
+from pyfarm.master.utility import (
+    JSONResponse, json_from_request, get_column_sets)
+
+ALL_AGENT_COLUMNS, REQUIRED_AGENT_COLUMNS = get_column_sets(AgentModel)
 
 # partial function(s) to assist in data conversion
 to_json = partial(json_from_request,
-                  all_keys=column_cache.all_columns(AgentModel),
+                  all_keys=ALL_AGENT_COLUMNS,
                   disallowed_keys=set(["id"]))
+
+logger = getLogger("api.agents")
 
 
 class AgentIndexAPI(MethodView):
+    # TODO: should return agent
     def post(self):
         """
-        Creates a new agent or updates and returns an existing one based on:
+        A ``POST`` to this endpoint will do one of two things:
 
-            * ip address (or hostname if address is not provided)
-            * amount of ram
-            * number of cpus
+            * update an existing agent and return the row
+            * create a new agent and return the row
+
+        In order to update an existing agent the following columns
+        must match an existing agent.  Generally speaking however, this
+        functionality is included solely limit the number of duplicate
+        agents:
+
+            * hostname
+            * port
+            * ram
+            * cpus
+
+        If the incoming request contains data (from the list above) that
+        matches an existing agent, the existing agent will be updated and
+        returned.  In all other cases, a ``POST`` to this endpoint will
+        result in the creation of a new agent.
+
+        .. http:post:: /api/v1/agents HTTP/1.1
+
+            **Request**
+
+            .. sourcecode:: http
+
+                POST /api/v1/agents HTTP/1.1
+                Accept: application/json
+
+                {
+                    "cpu_allocation": 1.0,
+                    "cpus": 14,
+                    "freeram": 133,
+                    "hostname": "agent1",
+                    "ip": "10.196.200.115",
+                    "port": 64994,
+                    "ram": 2157,
+                    "ram_allocation": 0.8,
+                    "state": 8
+                 }
+
+
+            **Response (existing agent updated)**
+
+            .. sourcecode:: http
+
+                HTTP/1.1 200 OK
+                Content-Type: application/json
+
+                {
+                    "cpu_allocation": 1.0,
+                    "cpus": 14,
+                    "freeram": 133,
+                    "hostname": "agent1",
+                    "id": 1,
+                    "ip": "10.196.200.115",
+                    "port": 64994,
+                    "ram": 2157,
+                    "ram_allocation": 0.8,
+                    "state": 8
+                 }
+
+            **Request**
+
+            .. sourcecode:: http
+
+                POST /api/v1/agents HTTP/1.1
+                Accept: application/json
+
+                {
+                    "cpu_allocation": 1.0,
+                    "cpus": 14,
+                    "freeram": 133,
+                    "hostname": "agent1",
+                    "ip": "10.196.200.115",
+                    "port": 64994,
+                    "ram": 2157,
+                    "ram_allocation": 0.8,
+                    "state": 8
+                 }
+
+            **Response (agent created)**
+
+            .. sourcecode:: http
+
+                HTTP/1.1 201 CREATED
+                Content-Type: application/json
+
+                {
+                    "cpu_allocation": 1.0,
+                    "cpus": 14,
+                    "freeram": 133,
+                    "hostname": "agent1",
+                    "id": 1,
+                    "ip": "10.196.200.115",
+                    "port": 64994,
+                    "ram": 2157,
+                    "ram_allocation": 0.8,
+                    "state": 8
+                 }
+
+        :statuscode 200: an existing agent was updated
+        :statuscode 201: a new agent was created
+        :statuscode 400: there was something wrong with the request (such as
+                         invalid columns being included)
         """
         # get json data from the request
         data = to_json(request)
         if isinstance(data, JSONResponse):
             return data
 
-        # TODO: check for existing hosts
-        # TODO: if host exists, update host
-        # TODO: if host does NOT exist, create new host
-        # TODO: return data
+        request_columns = set(data)
 
-        print data
-        return ""
+        # request did not include at least the required columns
+        if (request_columns != REQUIRED_AGENT_COLUMNS
+                and request_columns.issubset(REQUIRED_AGENT_COLUMNS)):
+            errorno, msg = APIError.MISSING_FIELDS
+            msg += ": %s" % list(REQUIRED_AGENT_COLUMNS - request_columns)
+            return JSONResponse((errorno, msg), status=BAD_REQUEST)
+
+        # request included some columns which don't exist
+        elif not request_columns.issubset(ALL_AGENT_COLUMNS):
+            errorno, msg = APIError.EXTRA_FIELDS_ERROR
+            extra_columns = list(request_columns - ALL_AGENT_COLUMNS)
+            msg = "the following columns do not exist: %s" % extra_columns
+            return JSONResponse((errorno, msg), status=BAD_REQUEST)
+
+        # update with our remote_addr from the request
+        data.update(remote_ip=request.remote_addr)
+
+        # check to see if there's already an existing agent with
+        # this information
+        existing_agent = AgentModel.query.filter_by(
+            hostname=data["hostname"], port=data["port"],
+            ram=data["ram"], cpus=data["cpus"]).first()
+
+        # agent already exists, try to update it
+        if existing_agent:
+            updated = {}
+            for key, value in data.iteritems():
+                if getattr(existing_agent, key) != value:
+                    setattr(existing_agent, key, value)
+                    updated[key] = value
+
+            # if not fields were updated, nothing to do here
+            if updated:
+                logger.debug(
+                    "updated agent %s: %s" % (existing_agent.id, updated))
+                db.session.add(existing_agent)
+                db.session.commit()
+
+            return JSONResponse(existing_agent.to_dict(), status=OK)
+
+        # didn't find an agent that matched the incoming data
+        # so we'll create one
+        else:
+            new_agent = AgentModel(**data)
+            db.session.add(new_agent)
+            db.session.commit()
+            agent_data = new_agent.to_dict()
+            logger.info("created agent %s: %s" % (new_agent.id, agent_data))
+            return JSONResponse(agent_data, status=CREATED)
 
 
 class SingleAgentAPI(MethodView):
