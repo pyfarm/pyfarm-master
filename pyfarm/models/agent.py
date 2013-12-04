37f4db5b897d22650e026248135929e96bd0a8d0
Guido Winkelmann <guido@ambient-entertainment.de>
2013-12-04 16:56:40 +0100

    Accept example hostnames again
    Fixes https://github.com/pyfarm/pyfarm-models/issues/21

--- pyfarm/models/agent.py
+++ pyfarm/models/agent.py
@@ -42,9 +42,9 @@ from pyfarm.models.core.cfg import (
     TABLE_AGENT_TAGS_DEPENDENCIES, TABLE_PROJECT_AGENTS, TABLE_PROJECT)
 
 
-REGEX_HOSTNAME = re.compile("^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*"
-                            "[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9]"
-                            "[A-Za-z0-9\-]*[A-Za-z0-9])[\.]$")
+REGEX_HOSTNAME = re.compile("^(?!-)[A-Z\d-]{1,63}(?<!-)"
+                            "(\.(?!-)[A-Z\d-]{1,63}(?<!-))*\.?$"
+                            , re.IGNORECASE)
 
 AgentTagDependency = db.Table(
     TABLE_AGENT_TAGS_DEPENDENCIES, db.metadata,
