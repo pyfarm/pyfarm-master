e05877f5d7fa807f720f5d28e5fad5655c612561
Oliver Palmer <oliverpalmer@opalmer.com>
2014-01-01 01:35:05 -0500

    removing tests related to old JSONResponse class

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -14,20 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from httplib import OK, BAD_REQUEST
+try:
+    from httplib import OK, BAD_REQUEST
+except ImportError:
+    from http.client import OK, BAD_REQUEST
 
 try:
     from json import loads
 except ImportError:
     from simplejson import loads
 
-from flask import Response
 from werkzeug.datastructures import ImmutableDict
-from utcore import ModelTestCase, TestCase
-from pyfarm.core.enums import APIError
+
+from .utcore import ModelTestCase, TestCase
 from pyfarm.master.utility import (
-    dumps, get_column_sets, JSONResponse, ReducibleDictionary,
-    json_from_request, TemplateDictionary)
+    get_column_sets, ReducibleDictionary, TemplateDictionary)
 from pyfarm.master.application import db
 from pyfarm.models.core.cfg import TABLE_PREFIX
 
@@ -59,26 +60,6 @@ class TestUtilityDB(ModelTestCase):
 
 
 class TestUtility(TestCase):
-    def test_dumps(self):
-        data = {"a": 0, "b": 1, "c": 2, "d": 4}
-        for indent in (2, 4, None):
-            dumped = dumps(data, indent=indent)
-            self.assertIsInstance(dumped, str)
-            self.assertDictEqual(loads(dumped), data)
-
-    def test_json_response(self):
-        data = {"a": 0, "b": 1, "c": 2, "d": 4}
-        self.assertTrue(issubclass(JSONResponse, Response))
-        response = JSONResponse()
-        self.assertEqual(response.content_type, "application/json")
-        self.assertEqual(response.status_code, OK)
-        response = JSONResponse(status=BAD_REQUEST)
-        self.assertEqual(response.status_code, BAD_REQUEST)
-        response = JSONResponse(data)
-        self.assertDictEqual(loads(response.data), data)
-        response = JSONResponse(ReducibleDictionary({"a": None}))
-        self.assertDictEqual(loads(response.data), {})
-
     def test_reducible_dictionary(self):
         source = {"a": None}
         data = ReducibleDictionary(source)
@@ -87,45 +68,6 @@ class TestUtility(TestCase):
         data.reduce()
         self.assertDictEqual(data, {})
         self.assertDictEqual(source, {"a": None})
-
-    def test_json_from_request(self):
-        request = FakeRequest({"a": None}, raise_error=True)
-        response = json_from_request(request)
-        self.assertIsInstance(response, JSONResponse)
-        self.assertEqual(response.status_code, BAD_REQUEST)
-        error_data = [
-            APIError.JSON_DECODE_FAILED[0],
-            "failed to decode any json data from the request: FAILED"]
-        self.assertEqual(response.status_code, BAD_REQUEST)
-        self.assertListEqual(loads(response.data), error_data)
-        request = FakeRequest({"a": "a"})
-        response = json_from_request(request)
-        self.assertIsInstance(response, dict)
-        self.assertDictEqual(response, {"a": "a"})
-        request = FakeRequest({"a": "a"})
-        response = json_from_request(request, required_keys=set(["c"]))
-        self.assertIsInstance(response, JSONResponse)
-        self.assertEqual(response.status_code, BAD_REQUEST)
-        error_data = [
-            APIError.MISSING_FIELDS[0],
-            "one or more of the expected fields were missing in the request.  Missing fields are: ['c']"]
-        self.assertListEqual(loads(response.data), error_data)
-        request = FakeRequest({"a": "a"})
-        response = json_from_request(request, all_keys=set(["z"]))
-        self.assertIsInstance(response, JSONResponse)
-        self.assertEqual(response.status_code, BAD_REQUEST)
-        error_data = [
-            APIError.EXTRA_FIELDS_ERROR[0],
-            "an unexpected number of fields or columns were provided.  Extra fields were: ['a']"]
-        self.assertListEqual(loads(response.data), error_data)
-        request = FakeRequest({"a": "a"})
-        response = json_from_request(request, disallowed_keys=set(["a"]))
-        self.assertIsInstance(response, JSONResponse)
-        self.assertEqual(response.status_code, BAD_REQUEST)
-        error_data = [
-            APIError.EXTRA_FIELDS_ERROR[0],
-            "an unexpected number of fields or columns were provided.  Extra fields were: ['a']"]
-        self.assertListEqual(loads(response.data), error_data)
 
     def test_template_dictionary(self):
         template = TemplateDictionary()
