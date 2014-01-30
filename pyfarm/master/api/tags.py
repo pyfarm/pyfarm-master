fada902e0eab27c514d24f020d2ad4f85142c591
Guido Winkelmann <guido@ambient-entertainment.de>
2014-01-30 20:10:41 +0100

    No need to cover import errors

--- pyfarm/master/api/tags.py
+++ pyfarm/master/api/tags.py
@@ -25,7 +25,7 @@ manage or query tags using JSON.
 
 try:
     from httplib import NOT_FOUND, NO_CONTENT, OK, CREATED, BAD_REQUEST
-except ImportError:
+except ImportError: # pragma: no cover
     from http.client import NOT_FOUND, NO_CONTENT, OK, CREATED, BAD_REQUEST
 
 from flask import Response, request, url_for
