21b0603306d46804ed08185b305ecfb2fff2cb6d
Oliver Palmer <oliverpalmer@opalmer.com>
2014-02-04 00:37:29 -0500

    adding tests for validate_with_model for #52

--- tests/test_master/test_utility.py
+++ tests/test_master/test_utility.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from json import dumps
+from functools import partial
+
 try:
     from httplib import OK, BAD_REQUEST
 except ImportError:
@@ -28,8 +31,10 @@ from pyfarm.master.testutil import BaseTestCase
 BaseTestCase.build_environment()
 
 from pyfarm.core.enums import NOTSET
+from pyfarm.models.core.mixins import UtilityMixins
 from pyfarm.master.utility import (
-    get_column_sets, ReducibleDictionary, TemplateDictionary, validate_json_type)
+    ReducibleDictionary, TemplateDictionary, validate_with_model)
+from pyfarm.master.entrypoints.main import load_error_handlers
 from pyfarm.master.application import db
 from pyfarm.models.core.cfg import TABLE_PREFIX
 
@@ -42,6 +47,13 @@ class ColumnSetTest(db.Model):
     d = db.Column(db.Integer, nullable=False, default=0)
 
 
+class ValidationTestModel(db.Model, UtilityMixins):
+    __tablename__ = "%s_validate_with_model_test" % TABLE_PREFIX
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    a = db.Column(db.Integer, nullable=False)
+    b = db.Column(db.Integer, nullable=False, default=1)
+
+
 class FakeRequest(object):
     def __init__(self, data, raise_error=False):
         self.data = data
@@ -51,13 +63,6 @@ class FakeRequest(object):
         if self.raise_error:
             raise ValueError("FAILED")
         return self.data
-
-
-class TestUtilityDB(BaseTestCase):
-    def test_get_column_sets(self):
-        all_columns, required_columns = get_column_sets(ColumnSetTest)
-        self.assertSetEqual(all_columns, set(["b", "c", "d"]))
-        self.assertSetEqual(required_columns, set(["c"]))
 
 
 class TestUtility(BaseTestCase):
@@ -81,46 +86,155 @@ class TestUtility(BaseTestCase):
         self.assertIsInstance(template(), ReducibleDictionary)
         self.assertIsInstance(template(reducible=False), dict)
 
-    def test_json_required_error(self):
-        g.error = 1
-        g.json = 1
 
-        @validate_json_type(None)
-        def foo():
-            return 1
+class TestValidateWithModel(BaseTestCase):
+    def setup_app(self):
+        super(TestValidateWithModel, self).setup_app()
+        load_error_handlers(self.app)
 
-        self.assertEqual(foo(), 1)
+    def setUp(self):
+        super(TestValidateWithModel, self).setUp()
+        self.post = partial(
+            self.client.post, headers={"Content-Type": "application/json"})
 
-    def test_json_required_json_notset(self):
+    def add_route(self, function, methods=None):
+        assert callable(function)
 
-        @validate_json_type(None)
-        def foo():
-            return 1
+        @self.app.route("/", methods=methods or ("POST", ))
+        def view():
+            return function()
 
-        with self.assertRaises(InternalServerError):
-            self.assertEqual(foo(), 1)
+    def test_no_data_to_decode(self):
+        @validate_with_model(ValidationTestModel)
+        def test():
+            return ""
 
-    def test_json_required_no_instance_check(self):
-        g.json = {}
+        self.add_route(test)
+        response = self.post("/")
+        self.assertIn("error", response.json)
+        self.assertEqual(response.json["error"], "no data to decode")
 
-        @validate_json_type(None)
-        def foo():
-            return
+    def test_invalid_type(self):
+        @validate_with_model(ValidationTestModel)
+        def test():
+            return ""
 
-        self.assertIsNone(foo())
+        self.add_route(test)
+        response = self.post("/", data=dumps(["foo", "bar"]))
+        self.assertIn("error", response.json)
+        self.assertEqual(
+            response.json["error"], "dictionary expected but got list instead")
 
-    def test_json_required_type_check(self):
-        g.json = {}
+    def test_fields_dont_exist(self):
+        @validate_with_model(ValidationTestModel)
+        def test():
+            return ""
 
-        @validate_json_type(dict)
-        def foo():
-            return
+        self.add_route(test)
+        response = self.post("/", data=dumps({"foobar": True}))
+        self.assertIn("error", response.json)
+        error_message = "request contains field(s) that do not exist " \
+                        "in %s: {'foobar'}" % ValidationTestModel.__tablename__
+        self.assertEqual(response.json["error"], error_message)
 
-        self.assertIsNone(foo())
+    def test_missing_required_fields(self):
+        @validate_with_model(ValidationTestModel)
+        def test():
+            return ""
 
-        @validate_json_type(int)
-        def foo():
-            return
+        self.add_route(test)
+        response = self.post("/", data=dumps({"b": 1}))
+        self.assertIn("error", response.json)
+        error_message = "request to %s is missing " \
+                        "field(s): {'a'}" % ValidationTestModel.__tablename__
+        self.assertEqual(response.json["error"], error_message)
 
-        with self.assertRaises(BadRequest):
-            foo()
+    def test_type_check(self):
+        @validate_with_model(ValidationTestModel)
+        def test():
+            return ""
+
+        self.add_route(test)
+        response = self.post("/", data=dumps({"a": "foobar"}))
+        self.assertIn("error", response.json)
+        error_message = "field 'a' has type <class 'str'> but we " \
+                        "expected type(s) <class 'int'>"
+        self.assertEqual(response.json["error"], error_message)
+
+    def test_custom_type_check_false(self):
+        with self.assertRaises(AssertionError):
+            @validate_with_model(ValidationTestModel, type_checks={"a"})
+            def test():
+                return ""
+
+        def a_type_check(value):
+            return False
+
+        @validate_with_model(
+            ValidationTestModel, type_checks={"a": a_type_check})
+        def test():
+            return ""
+
+        self.add_route(test)
+        response = self.post("/", data=dumps({"a": 1}))
+        self.assert_bad_request(response)
+
+    def test_custom_type_check_true(self):
+        with self.assertRaises(AssertionError):
+            @validate_with_model(ValidationTestModel, type_checks={"a"})
+            def test():
+                return ""
+
+        def a_type_check(value):
+            return True
+
+        @validate_with_model(
+            ValidationTestModel, type_checks={"a": a_type_check})
+        def test():
+            return ""
+
+        self.add_route(test)
+        response = self.post("/", data=dumps({"a": 1}))
+        self.assert_ok(response)
+
+    def test_custom_type_check_with_custom_error(self):
+        with self.assertRaises(AssertionError):
+            @validate_with_model(ValidationTestModel, type_checks={"a"})
+            def test():
+                return ""
+
+        def a_type_check(value):
+            g.error = "bad"
+            return False
+
+        @validate_with_model(
+            ValidationTestModel, type_checks={"a": a_type_check})
+        def test():
+            return ""
+
+        self.add_route(test)
+        response = self.post("/", data=dumps({"a": 1}))
+        self.assert_bad_request(response)
+        self.assertEqual(response.json, {"error": "bad"})
+
+    def test_custom_type_check_invalid_output(self):
+        with self.assertRaises(AssertionError):
+            @validate_with_model(ValidationTestModel, type_checks={"a"})
+            def test():
+                return ""
+
+        def a_type_check(value):
+            return None
+
+        @validate_with_model(
+            ValidationTestModel, type_checks={"a": a_type_check})
+        def test():
+            return ""
+
+        self.add_route(test)
+        response = self.post("/", data=dumps({"a": 1}))
+        self.assert_internal_server_error(response)
+        self.assertEqual(
+            response.json,
+            {"error": "expected custom type check function for "
+                      "'a' to return True or False"})
