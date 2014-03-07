62d323b3e12ceb78843f638dba93121d1d28138a
Guido Winkelmann <guido@ambient-entertainment.de>
2014-03-07 15:44:13 +0100

    Style fix: rename variables

--- pyfarm/master/api/jobs.py
+++ pyfarm/master/api/jobs.py
@@ -55,7 +55,7 @@ class ObjectNotFound(Exception):
     pass
 
 
-def parse_requirements(requirements_list):
+def parse_requirements(requirements):
     """
     Takes a list dicts specifying a software and optional min- and max-versions
     and returns a list of :class:`JobRequirement` objects.
@@ -78,16 +78,16 @@ def parse_requirements(requirements_list):
     :raises ObjectNotFound:
         Raised if the referenced software or version was not found
     """
-    if not isinstance(requirements_list, list):
+    if not isinstance(requirements, list):
         raise TypeError("software_requirements must be a list")
 
     out = []
-    for req in requirements_list:
+    for entry in requirements:
         if not isinstance(req, dict):
             raise TypeError("Every software_requirement must be a dict")
 
         requirement = JobSoftwareRequirement()
-        software_name = req.pop("software", None)
+        software_name = entry.pop("software", None)
         if software_name is None:
             raise ValueError("Software requirement does not specify a software.")
         software = Software.query.filter_by(software=software_name).first()
@@ -95,7 +95,7 @@ def parse_requirements(requirements_list):
             raise ObjectNotFound("Software %s not found" % software_name)
         requirement.software = software
 
-        min_version_str = req.pop("min_version", None)
+        min_version_str = entry.pop("min_version", None)
         if min_version_str is not None:
             min_version = SoftwareVersion.query.filter(
                 SoftwareVersion.software == software,
@@ -105,7 +105,7 @@ def parse_requirements(requirements_list):
                                         (software_name, min_version_str))
             requirement.min_version = min_version
 
-        max_version_str = req.pop("max_version", None)
+        max_version_str = entry.pop("max_version", None)
         if max_version_str is not None:
             max_version = SoftwareVersion.query.filter(
                 SoftwareVersion.software == software,
@@ -115,9 +115,9 @@ def parse_requirements(requirements_list):
                                      (software_name, max_version_str))
             requirement.max_version = max_version
 
-        if req:
+        if entry:
             raise TypeError("Unexpected keys in software requirement: %r" %
-                            req.keys())
+                            entry.keys())
 
         out.append(requirement)
     return out
