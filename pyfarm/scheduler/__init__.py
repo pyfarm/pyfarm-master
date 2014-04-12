d41a78cd7d43c7b6d32de56239012e86f716a6bd
Oliver Palmer <oliverpalmer@opalmer.com>
2014-04-12 00:48:48 -0400

    adding stub documentation

--- pyfarm/scheduler/__init__.py
+++ pyfarm/scheduler/__init__.py
@@ -1,6 +1,7 @@
 # No shebang line, this module is meant to be imported
 #
 # Copyright 2014 Ambient Entertainment GmbH & Co. KG
+# Copyright 2014 Oliver Palmer
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,3 +14,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""
+Scheduler
+=========
+
+This package contains the components used by PyFarm's master to schedule
+tasks, assign work to agents, and other periodic tasks that can't be performed
+inside of the web application.
+"""
