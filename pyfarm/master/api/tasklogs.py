078e923b79f6e7972af065df5ee7e56f3d6bcb9b
Guido Winkelmann <guido@ambient-entertainment.de>
2014-09-03 19:02:12 +0200

    Add docstrings to tasklogs API

--- pyfarm/master/api/tasklogs.py
+++ pyfarm/master/api/tasklogs.py
@@ -56,7 +56,39 @@ except OSError as e:  # pragma: no cover
 
 
 class LogsInTaskAttemptsIndexAPI(MethodView):
+
     def get(self, job_id, task_id, attempt):
+        """
+        A ``GET`` to this endpoint will return a list of all known logs that are
+        associated with this attempt at running this task
+
+        .. http:get:: /api/v1/jobs/<job_id>/tasks/<task_id>/attempts/<attempt>/logs/ HTTP/1.1
+
+            **Request**
+
+            .. sourcecode:: http
+
+                GET /api/v1/jobs/4/tasks/1300/attempts/5/logs/ HTTP/1.1
+                Accept: application/json
+
+            **Response**
+
+            .. sourcecode:: http
+
+                HTTP/1.1 200 OK
+                Content-Type: application/json
+
+                [
+                    {
+                        "agent_id": 1,
+                        "created_on": "2014-09-03T10:58:59.754880",
+                        "identifier": "2014-09-03_10-58-59_4_4ee02475335911e4a935c86000cbf5fb.csv"
+                    }
+                ]
+
+        :statuscode 200: no error
+        :statuscode 404: the specified task was not found
+        """
         task = Task.query.filter_by(id=task_id, job_id=job_id).first()
         if not task:
             return jsonify(error="Specified task not found"), NOT_FOUND
@@ -76,6 +108,51 @@ class LogsInTaskAttemptsIndexAPI(MethodView):
 
     @validate_with_model(TaskLog)
     def post(self, job_id, task_id, attempt):
+        """
+        A ``POST`` to this endpoint will register a new logfile with the given
+        attempt at running the given task
+
+        A logfile has an identifier which must be unique in the system.  If two
+        tasks get assigned a logfile with the same id, it is considered to be the
+        same log.
+
+        .. http:post:: /api/v1/jobs/4/tasks/1300/attempts/5/logs/ HTTP/1.1
+
+            **Request**
+
+            .. sourcecode:: http
+
+                POST /api/v1/tags/ HTTP/1.1
+                Content-Type: application/json
+
+                {
+                    "identifier": "2014-09-03_10-58-59_4_4ee02475335911e4a935c86000cbf5fb.csv",
+                    "agent_id": 1
+                }
+
+            **Response**
+
+            .. sourcecode:: http
+
+                HTTP/1.1 201 CREATED
+                Content-Type: application/json
+
+                {
+                    "identifier": "2014-09-03_10-58-59_4_4ee02475335911e4a935c86000cbf5fb.csv",
+                    "agent_id": 1
+                    "created_on": "2014-09-03T10:59:05.103005",
+                    "id": 148
+                }
+
+
+        :statuscode 201: the association between this task attempt and logfile
+                         has been created
+        :statuscode 400: there was something wrong with the request (such as
+                         invalid columns being included)
+        :statuscode 404: the specified task does not exist
+        :statuscode 409: the specified log was already registered on the
+                         specified task
+        """
         task = Task.query.filter_by(id=task_id, job_id=job_id).first()
         if not task:
             return jsonify(error="Specified task not found"), NOT_FOUND
@@ -111,6 +188,36 @@ class LogsInTaskAttemptsIndexAPI(MethodView):
 
 class SingleLogInTaskAttempt(MethodView):
     def get(self, job_id, task_id, attempt, log_identifier):
+        """
+        A ``GET`` to this endpoint will return metadata about the specified
+        logfile
+
+        .. http:get:: /api/v1/jobs/<job_id>/tasks/<task_id>/attempts/<attempt>/logs/<log_identifier> HTTP/1.1
+
+            **Request**
+
+            .. sourcecode:: http
+
+                GET /api/v1/jobs/4/tasks/1300/attempts/5/logs/2014-09-03_10-58-59_4_4ee02475335911e4a935c86000cbf5fb.csv HTTP/1.1
+                Accept: application/json
+
+            **Response**
+
+            .. sourcecode:: http
+
+                HTTP/1.1 200 OK
+                Content-Type: application/json
+
+                {
+                    "id": 147,
+                    "identifier": "2014-09-03_10-58-59_4_4ee02475335911e4a935c86000cbf5fb.csv",
+                    "created_on": "2014-09-03T10:58:59.754880",
+                    "agent_id": 1
+                }
+
+        :statuscode 200: no error
+        :statuscode 404: task or logfile not found
+        """
         task = Task.query.filter_by(id=task_id, job_id=job_id).first()
         if not task:
             return jsonify(error="Specified task not found"), NOT_FOUND
@@ -131,6 +238,35 @@ class SingleLogInTaskAttempt(MethodView):
 
 class TaskLogfileAPI(MethodView):
     def get(self, job_id, task_id, attempt, log_identifier):
+        """
+        A ``GET`` to this endpoint will return the actual logfile or a redirect
+        to it.
+
+        .. http:get:: /api/v1/jobs/<job_id>/tasks/<task_id>/attempts/<attempt>/logs/<log_identifier>/logfile HTTP/1.1
+
+            **Request**
+
+            .. sourcecode:: http
+
+                GET /api/v1/jobs/4/tasks/1300/attempts/5/logs/2014-09-03_10-58-59_4_4ee02475335911e4a935c86000cbf5fb.csv/logfile HTTP/1.1
+                Accept: text/csv
+
+            **Response**
+
+            .. sourcecode:: http
+
+                HTTP/1.1 200 OK
+                Content-Type: text/csv
+
+                <Content of the logfile>
+
+        :statuscode 200: no error
+        :statuscode 307: The logfile can be found in another location at this
+                         point in time. Independent future requests for the same
+                         logfile should continue using the original URL
+        :statuscode 400: the specified logfile identifier is not acceptable
+        :statuscode 404: task or logfile not found
+        """
         task = Task.query.filter_by(id=task_id, job_id=job_id).first()
         if not task:
             return jsonify(error="Specified task not found"), NOT_FOUND
@@ -161,6 +297,30 @@ class TaskLogfileAPI(MethodView):
                             TEMPORARY_REDIRECT)
 
     def put(self, job_id, task_id, attempt, log_identifier):
+        """
+        A ``PUT`` to this endpoint will upload the request's body as the
+        specified logfile
+
+        .. http:put:: /api/v1/jobs/<job_id>/tasks/<task_id>/attempts/<attempt>/logs/<log_identifier>/logfile HTTP/1.1
+
+            **Request**
+
+            .. sourcecode:: http
+
+                PUT /api/v1/jobs/4/tasks/1300/attempts/5/logs/2014-09-03_10-58-59_4_4ee02475335911e4a935c86000cbf5fb.csv/logfile HTTP/1.1
+
+                <content of the logfile>
+
+            **Response**
+
+            .. sourcecode:: http
+
+                HTTP/1.1 201 CREATED
+
+        :statuscode 201: lofile was uploaded
+        :statuscode 400: the specified logfile identifier is not acceptable
+        :statuscode 404: task or logfile not found
+        """
         task = Task.query.filter_by(id=task_id, job_id=job_id).first()
         if not task:
             return jsonify(error="Specified task not found"), NOT_FOUND
