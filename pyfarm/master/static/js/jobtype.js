536cb23107843841b091f8afcc9a5df95d8537d1
Guido Winkelmann <guido@ambient-entertainment.de>
2014-10-14 10:42:12 +0200

    Remove leftover debug statements

--- pyfarm/master/static/js/jobtype.js
+++ pyfarm/master/static/js/jobtype.js
@@ -5,10 +5,8 @@ $(document).ready(function() {
         $("#minimum_version, #maximum_version").append(empty_option);
         var software_id = $("#software").val()
         $.getJSON("/api/v1/software/"+software_id+"/versions/", function(r) {
-            console.log(r);
             var empty_option = $("<option></option>");
             for(var i = 0; i < r.length ; i++) {
-                console.log(r[i]);
                 var option = $('<option value="'+r[i]['id']+'">'+r[i]['version']+'</option>');
                 $("#minimum_version, #maximum_version").append(option);
             }
