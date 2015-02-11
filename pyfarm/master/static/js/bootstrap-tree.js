63c9b828010e307888a46249f8ca93674109dd46
Guido Winkelmann <guido@ambient-entertainment.de>
2015-02-11 22:45:52 +0100

    Make Jobqueue view start collapsed
    This makes for a better overview if you have a lot of queues and jobs.

--- pyfarm/master/static/js/bootstrap-tree.js
+++ pyfarm/master/static/js/bootstrap-tree.js
@@ -1,5 +1,10 @@
 $(function () {
-    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Expand this branch');
+
+    var children = $('.tree li.parent_li').find(' > ul > li');
+    children.hide('fast');
+    $('.tree li.parent_li > span').find(' > i').addClass('glyphicon-plus-sign').removeClass('glyphicon-minus-sign');
+
     $('.tree li.parent_li > span').on('click', function (e) {
         var children = $(this).parent('li.parent_li').find(' > ul > li');
         if (children.is(":visible")) {
