b467204d7433eeb982de659a5222b93f9115e964
Guido Winkelmann <guido@ambient-entertainment.de>
2015-02-11 22:45:15 +0100

    Fix class names for icons in trees for Bootstrap 3

--- pyfarm/master/static/js/bootstrap-tree.js
+++ pyfarm/master/static/js/bootstrap-tree.js
@@ -4,10 +4,10 @@ $(function () {
         var children = $(this).parent('li.parent_li').find(' > ul > li');
         if (children.is(":visible")) {
             children.hide('fast');
-            $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon-plus-sign').removeClass('glyphicon-minus-sign');
         } else {
             children.show('fast');
-            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus-sign').removeClass('glyphicon-plus-sign');
         }
         e.stopPropagation();
     });
