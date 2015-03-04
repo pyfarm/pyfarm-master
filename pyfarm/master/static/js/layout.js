aac45423d057be59ba7d9f8ada03705f3f32442e
Guido Winkelmann <guido@ambient-entertainment.de>
2015-03-05 00:07:40 +0100

    Filter agents by multiple states

--- pyfarm/master/static/js/layout.js
+++ pyfarm/master/static/js/layout.js
@@ -1,3 +1,11 @@
 function hide_parent(element) {
     element.parentNode.style.display='none';
     };
+
+$(document).ready(function() {
+    $('.dropdown-menu').on('click', function(e) {
+        if($(this).hasClass('dropdown-menu-form')) {
+            e.stopPropagation();
+        }
+    });
+});
