d78e3ad9bee10ee7c01e0b0996d4d4a71abb0cbe
Oliver Palmer <oliverpalmer@opalmer.com>
2015-04-18 12:02:05 -0400

    specialize the config a bit to handle tempdir

--- pyfarm/master/config.py
+++ pyfarm/master/config.py
@@ -24,47 +24,58 @@ compatibility for some environment variables.
 """
 
 import os
+import tempfile
 from functools import partial
+from errno import EEXIST
 
 from pyfarm.core.config import (
-    Configuration, read_env_int, read_env, read_env_bool)
+    Configuration as _Configuration, read_env_int, read_env, read_env_bool)
+
+try:
+    WindowsError
+except NameError:  # pragma: no cover
+    WindowsError = OSError
 
 
-def load_environment():
-    """
-    Provides a mapping of environment variable values to their
-    configuration counterparts.
-    """
-    read_env_no_log = partial(read_env, log_result=False)
-    output = {}
-    environment = {
-        "secret_key": ("PYFARM_SECRET_KEY", read_env_no_log),
-        "autocreate_users": ("PYFARM_AUTOCREATE_USERS", read_env_bool),
-        "autocreate_user_domain": (
-            "PYFARM_AUTO_USERS_DEFAULT_DOMAIN", read_env_bool),
-        "default_job_delete_time": (
-            "PYFARM_DEFAULT_JOB_DELETE_TIME", read_env_int),
-        "login_disabled": ("PYFARM_LOGIN_DISABLED", read_env_bool),
-        "pretty_json": ("PYFARM_JSON_PRETTY", read_env_bool),
-        "echo_sql": ("PYFARM_SQL_ECHO", read_env_bool),
-        "database": ("PYFARM_DATABASE_URI", read_env_no_log),
-        "timestamp_format": ("PYFARM_TIMESTAMP_FORMAT", read_env),
-        "allow_agents_from_loopback": (
-            "PYFARM_DEV_ALLOW_AGENT_LOOPBACK_ADDRESSES", read_env_bool),
-        "agent_updates_dir": ("PYFARM_AGENT_UPDATES_DIR", read_env),
-        "agent_updates_webdir": ("PYFARM_AGENT_UPDATES_WEBDIR", read_env),
-        "farm_name": ("PYFARM_FARM_NAME", read_env)
-    }
+class Configuration(_Configuration):
+    def __init__(self):
+        super(Configuration, self).__init__("pyfarm.master")
+        read_env_no_log = partial(read_env, log_result=False)
+        overrides = {
+            "secret_key": ("PYFARM_SECRET_KEY", read_env_no_log),
+            "autocreate_users": ("PYFARM_AUTOCREATE_USERS", read_env_bool),
+            "autocreate_user_domain": (
+                "PYFARM_AUTO_USERS_DEFAULT_DOMAIN", read_env_bool),
+            "default_job_delete_time": (
+                "PYFARM_DEFAULT_JOB_DELETE_TIME", read_env_int),
+            "login_disabled": ("PYFARM_LOGIN_DISABLED", read_env_bool),
+            "pretty_json": ("PYFARM_JSON_PRETTY", read_env_bool),
+            "echo_sql": ("PYFARM_SQL_ECHO", read_env_bool),
+            "database": ("PYFARM_DATABASE_URI", read_env_no_log),
+            "timestamp_format": ("PYFARM_TIMESTAMP_FORMAT", read_env),
+            "allow_agents_from_loopback": (
+                "PYFARM_DEV_ALLOW_AGENT_LOOPBACK_ADDRESSES", read_env_bool),
+            "agent_updates_dir": ("PYFARM_AGENT_UPDATES_DIR", read_env),
+            "agent_updates_webdir": ("PYFARM_AGENT_UPDATES_WEBDIR", read_env),
+            "farm_name": ("PYFARM_FARM_NAME", read_env)
+        }
 
-    for config_key, (env_key, get_env) in environment.items():
-        if env_key in os.environ:
-            output[config_key] = get_env(env_key)
+        self.load(environment=overrides)
 
-    return environment
+        # TODO: Specialize per platform? gettempdir() is not constant on OS X.
+        if self["tempdir"] is None:
+            self["tempdir"] = tempfile.gettempdir()
 
+        # Make sure tempdir exists.  In the default case
+        # this should always be true.  In the non-default
+        # case we won't know unless we try.
+        try:
+            os.makedirs(self["tempdir"])
+        except (OSError, IOError, WindowsError) as error:
+            if error.errno != EEXIST:
+                raise
 
 try:
     config
 except NameError:  # pragma: no cover
-    config = Configuration("pyfarm.master")
-    config.load(environment=load_environment())
+    config = Configuration()
