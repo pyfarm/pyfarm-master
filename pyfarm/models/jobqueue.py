90e0dc667e9f0b72748bc47b3ee7a4af935ea275
Oliver Palmer <oliverpalmer@opalmer.com>
2015-04-20 00:08:03 -0400

    conversion to config, style cleanup

--- pyfarm/models/jobqueue.py
+++ pyfarm/models/jobqueue.py
@@ -23,26 +23,23 @@ Model for job queues
 """
 
 from sys import maxsize
-from textwrap import dedent
 from functools import reduce
 from logging import DEBUG
 
 from sqlalchemy import event, distinct, or_
 from sqlalchemy.schema import UniqueConstraint
 
-from pyfarm.core.config import read_env_int, read_env_bool
-
 from pyfarm.core.logger import getLogger
 from pyfarm.core.enums import WorkState, _WorkState
 from pyfarm.master.application import db
-from pyfarm.models.core.cfg import (
-    TABLE_JOB_QUEUE, MAX_JOBQUEUE_NAME_LENGTH, MAX_JOBQUEUE_PATH_LENGTH)
+from pyfarm.master.config import config
 from pyfarm.models.core.mixins import UtilityMixins, ReprMixin
 from pyfarm.models.core.types import id_column, IDTypeWork
 
-PREFER_RUNNING_JOBS = read_env_bool("PYFARM_PREFER_RUNNING_JOBS", True)
+PREFER_RUNNING_JOBS = config.get("queue_prefer_running_jobs")
 logger = getLogger("pf.models.jobqueue")
-if read_env_bool("PYFARM_JOBQUEUE_DEBUG", True):
+
+if config.get("debug_queue"):
     logger.setLevel(DEBUG)
 
 
@@ -51,57 +48,72 @@ class JobQueue(db.Model, UtilityMixins, ReprMixin):
     Stores information about a job queue. Used for flexible, configurable
     distribution of computing capacity to jobs.
     """
-    __tablename__ = TABLE_JOB_QUEUE
+    __tablename__ = config.get("table_job_queue")
     __table_args__ = (UniqueConstraint("parent_jobqueue_id", "name"),)
 
     REPR_COLUMNS = ("id", "name")
 
     id = id_column(IDTypeWork)
-    parent_jobqueue_id = db.Column(IDTypeWork,
-                                   db.ForeignKey("%s.id" % TABLE_JOB_QUEUE),
-                                   nullable=True,
-                                   doc="The parent queue of this queue. If "
-                                       "NULL, this is a top level queue.")
-    name = db.Column(db.String(MAX_JOBQUEUE_NAME_LENGTH), nullable=False)
-    minimum_agents = db.Column(db.Integer, nullable=True,
-                          doc=dedent("""
-                          The scheduler will try to assign at least this number
-                          of agents to jobs in or below this queue as long as it
-                          can use them, before any other considerations."""))
-    maximum_agents = db.Column(db.Integer, nullable=True,
-                          doc=dedent("""
-                          The scheduler will never assign more than this number
-                          of agents to jobs in or below this queue."""))
-    priority = db.Column(db.Integer, nullable=False,
-                         default=read_env_int(
-                                   "PYFARM_QUEUE_DEFAULT_PRIORITY", 0),
-                         doc=dedent("""
-                             The priority of this job queue.
-                             The scheduler will not assign any nodes to other
-                             job queues or jobs with the same parent and a lower
-                             priority as long as this one can still use nodes.
-                             The minimum_agents column takes precedence over
-                             this."""))
-    weight = db.Column(db.Integer, nullable=False,
-                       default=read_env_int(
-                                   "PYFARM_QUEUE_DEFAULT_WEIGHT", 10),
-                       doc=dedent("""
-                            The weight of this job queue.
-                            The scheduler will distribute available agents
-                            between jobs and job queues in the same queue
-                            in proportion to their weights.
-                            """))
-    fullpath = db.Column(db.String(MAX_JOBQUEUE_PATH_LENGTH),
-                         doc="The path of this jobqueue.  This column is a "
-                             "database denormalization.  It is technically "
-                             "redundant, but faster to access than recursively "
-                             "querying all parent queues.  If set to NULL, the "
-                             "path must be computed by recursively querying "
-                             "the parent queues.")
-    parent = db.relationship("JobQueue",
-                             remote_side=[id],
-                             backref=db.backref("children", lazy="dynamic"),
-                             doc="Relationship between this queue its parent")
+
+    parent_jobqueue_id = db.Column(
+        IDTypeWork,
+        db.ForeignKey("%s.id" % config.get("table_job_queue")),
+        nullable=True,
+        doc="The parent queue of this queue. If NULL, this is a top "
+            "level queue.")
+
+    name = db.Column(
+        db.String(config.get("max_queue_name_length")),
+        nullable=False,
+        doc="The name of the job queue")
+
+    minimum_agents = db.Column(
+        db.Integer,
+        nullable=True,
+        doc="The scheduler will try to assign at least this number of "
+            "agents to jobs in or below this queue as long as it "
+            "can use them, before any other considerations.")
+
+    maximum_agents = db.Column(
+        db.Integer,
+        nullable=True,
+        doc="The scheduler will never assign more than this number of "
+            "agents to jobs in or below this queue.")
+
+    priority = db.Column(
+        db.Integer,
+        nullable=False,
+        default=config.get("queue_default_priority"),
+        doc="The priority of this job queue. The scheduler will not "
+            "assign any nodes to other job queues or jobs with the "
+            "same parent and a lower priority as long as this one "
+            "can still use nodes. The minimum_agents column takes "
+            "precedence over this.")
+
+    weight = db.Column(
+        db.Integer,
+        nullable=False,
+        default=config.get("queue_default_weight"),
+        doc="The weight of this job queue. The scheduler will "
+            "distribute available agents between jobs and job "
+            "queues in the same queue in proportion to their "
+            "weights.")
+
+    fullpath = db.Column(
+        db.String(config.get("max_queue_path_length")),
+        doc="The path of this jobqueue.  This column is a "
+            "database denormalization.  It is technically "
+            "redundant, but faster to access than recursively "
+            "querying all parent queues.  If set to NULL, the "
+            "path must be computed by recursively querying "
+            "the parent queues.")
+
+    parent = db.relationship(
+        "JobQueue",
+        remote_side=[id],
+        backref=db.backref("children", lazy="dynamic"),
+        doc="Relationship between this queue its parent")
+
 
     def path(self):
         # Import here instead of at the top to break circular dependency
