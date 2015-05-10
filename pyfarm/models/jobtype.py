72aeca8a863f7a943f64bb19c940a1d22b868a5d
Oliver Palmer <oliverpalmer@opalmer.com>
2015-05-10 13:45:26 -0400

    reformat JobTypeVersion

--- pyfarm/models/jobtype.py
+++ pyfarm/models/jobtype.py
@@ -102,53 +102,71 @@ class JobType(db.Model, UtilityMixins, ReprMixin):
 
 
 class JobTypeVersion(db.Model, UtilityMixins, ReprMixin):
+    """
+    Defines a specific jobtype version.
+    """
     __tablename__ = TABLE_JOB_TYPE_VERSION
     __table_args__ = (UniqueConstraint("jobtype_id", "version"),)
 
-    REPR_COLUMNS = (
-        "id", "jobtype_id", "version")
-    id = id_column(IDTypeWork)
-    jobtype_id = db.Column(IDTypeWork,
-                           db.ForeignKey("%s.id" % TABLE_JOB_TYPE),
-                           nullable=False,
-                           doc="The jobtype this version belongs to")
-    version = db.Column(db.Integer, nullable=False, doc="The version number")
-    max_batch = db.Column(db.Integer,
-                          default=read_env_int(
-                              "JOBTYPE_DEFAULT_MAX_BATCH",
-                              read_env_int("PYFARM_QUEUE_MAX_BATCH", 1)),
-                          doc=dedent("""
-                          When the queue runs, this is the maximum number of
-                          tasks that the queue can select to assign to a single
-                          agent.  If left empty, no maximum applies"""))
-    batch_contiguous = db.Column(db.Boolean,
-                                 default=read_env_bool(
-                                     "JOBTYPE_DEFAULT_BATCH_CONTIGUOUS", True),
-                                 doc=dedent("""
-                                 If True then the queue will be forced to batch
-                                 numerically contiguous tasks only for this
-                                 job type.  For example if True it would batch
-                                 frames 1, 2, 3, 4 together but not 2, 4, 6,
-                                 8.  If this column is False however the queue
-                                 will batch non-contiguous tasks too."""))
-    classname = db.Column(db.String(MAX_JOBTYPE_LENGTH), nullable=True,
-                          doc=dedent("""
-                          The name of the job class contained within the file
-                          being loaded.  This field may be null but when it's
-                          not provided job type name will be used instead."""))
-    code = db.Column(db.UnicodeText, nullable=False,
-                     doc="The source code of the job type")
+    REPR_COLUMNS = ("id", "jobtype_id", "version")
 
-    jobtype = db.relationship("JobType",
-                              backref=db.backref("versions", lazy="dynamic",
-                                                 cascade="all, delete-orphan"),
-                              doc=dedent("""
-                                  Relationship between this version and the
-                                  :class:`JobType` it belongs to"""))
-    jobs = db.relationship("Job", backref="jobtype_version", lazy="dynamic",
-                           doc=dedent("""
-                           Relationship between this jobtype version and
-                           :class:`.Job` objects."""))
+    id = id_column(IDTypeWork)
+
+    jobtype_id = db.Column(
+        IDTypeWork,
+        db.ForeignKey("%s.id" % TABLE_JOB_TYPE),
+        nullable=False,
+        doc="The jobtype this version belongs to")
+
+    version = db.Column(
+        db.Integer,
+        nullable=False,
+        doc="The version number")
+
+    max_batch = db.Column(
+        db.Integer,
+        default=read_env_int(
+            "JOBTYPE_DEFAULT_MAX_BATCH",
+            read_env_int("PYFARM_QUEUE_MAX_BATCH", 1)),
+        doc="When the queue runs, this is the maximum number of tasks "
+            "that the queue can select to assign to a single"
+            "agent.  If left empty, no maximum applies")
+
+    batch_contiguous = db.Column(
+        db.Boolean,
+        default=read_env_bool("JOBTYPE_DEFAULT_BATCH_CONTIGUOUS", True),
+        doc="If True then the queue will be forced to batch"
+            "numerically contiguous tasks only for this job type.  "
+            "For example if True it would batch frames 1, 2, 3, 4 "
+            "together but not 2, 4, 6, 8.  If this column is False "
+            "however the queue will batch non-contiguous tasks too.")
+
+    classname = db.Column(
+        db.String(MAX_JOBTYPE_LENGTH),
+        nullable=True,
+        doc="The name of the job class contained within the file being "
+            "loaded.  This field may be null but when it's not provided "
+            "job type name will be used instead.")
+
+    code = db.Column(
+        db.UnicodeText,
+        nullable=False,
+        doc="The source code of the job type")
+
+    #
+    # Relationships
+    #
+    jobtype = db.relationship(
+        "JobType",
+        backref=db.backref(
+            "versions", lazy="dynamic", cascade="all, delete-orphan"),
+        doc="Relationship between this version and the "
+            ":class:`JobType` it belongs to""")
+
+    jobs = db.relationship(
+        "Job", backref="jobtype_version", lazy="dynamic",
+        doc="Relationship between this jobtype version and "
+            ":class:`.Job` objects.")
 
     @validates("max_batch")
     def validate_max_batch(self, key, value):
