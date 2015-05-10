e1f2f45678d43f966817b4c7d1965b6bc02c9fab
Oliver Palmer <oliverpalmer@opalmer.com>
2015-05-10 13:11:45 -0400

    add copyright

--- pyfarm/models/gpu.py
+++ pyfarm/models/gpu.py
@@ -1,6 +1,7 @@
 # No shebang line, this module is meant to be imported
 #
 # Copyright 2014 Ambient Entertainment GmbH & Co. KG
+# Copyright 2015 Oliver Palmer
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
