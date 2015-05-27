c6724b2ed1aa457f6c27134ea2d60bede46bb071
Guido Winkelmann <guido@ambient-entertainment.de>
2015-05-27 14:16:57 +0200

    Fix installing data files in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # No shebang line, this module is meant to be imported
 #
 # Copyright 2013 Oliver Palmer
+# Copyright 2015 Ambient Entertainment GmbH & Co. KG
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,7 +22,7 @@ assert sys.version_info[0:2] >= (2, 7), "Python 2.7 or higher is required"
 
 import os
 from os import walk
-from os.path import isfile, join, commonprefix
+from os.path import isfile, join, relpath
 from setuptools import setup
 
 # Version requirements explanations:
@@ -54,14 +55,12 @@ else:
     long_description = ""
 
 
-def get_package_data(*package_data_roots):
-    package_root = commonprefix(package_data_roots)
-
+def get_package_data(package_root, *package_data_roots):
     output = []
     for top in package_data_roots:
         for root, dirs, files in walk(top):
             for filename in files:
-                output.append(join(root, filename).split(package_root)[-1][1:])
+                output.append(relpath(join(root, filename), package_root))
 
     return output
 
@@ -80,26 +79,20 @@ setup(
     include_package_data=True,
     package_data={
         "pyfarm.master": get_package_data(
+            join("pyfarm", "master"),
             join("pyfarm", "master", "etc"),
             join("pyfarm", "master", "static"),
-            join("pyfarm", "master", "templates"),
-            join("pyfarm", "master", "api", "templates"),
-            join("pyfarm", "master", "api", "static")
+            join("pyfarm", "master", "templates")
         ),
         "pyfarm.models": get_package_data(
+            join("pyfarm", "models"),
             join("pyfarm", "models", "etc")
         ),
         "pyfarm.scheduler": get_package_data(
+            join("pyfarm", "scheduler"),
             join("pyfarm", "scheduler", "etc")
         )
     },
-    data_files=[
-        ("etc/pyfarm", [
-            "pyfarm/master/etc/master.yml",
-            "pyfarm/models/etc/models.yml",
-            "pyfarm/scheduler/etc/scheduler.yml"
-        ])
-    ],
     entry_points={
         "console_scripts": [
             "pyfarm-master = pyfarm.master.entrypoints:run_master",
